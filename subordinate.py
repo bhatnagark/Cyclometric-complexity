3ee7b7ac825d00e36c39d0a8d6eeab79537039cc
bhatnagark <32340359+bhatnagark@users.noreply.github.com>
2017-12-11 23:31:18 +0000

    subordinate initialisation

---
+++ subordinate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 23:26:06 2017
+
+@author: kshitijbhatnagar
+"""
+
+import json, requests, subprocess
+
+if __name__ == "__main__":
+    work()
