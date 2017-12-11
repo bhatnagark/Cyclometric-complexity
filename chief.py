b656e5e0b768702c01fa4a028d77cc739f75f417
bhatnagark <32340359+bhatnagark@users.noreply.github.com>
2017-12-11 23:25:02 +0000

    get repository function

--- chief.py
+++ chief.py
@@ -11,6 +11,38 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import json, requests, time, getpass
 
+class repository:
+    def __init__(self):
+        #global server object
+        global chiefS
+        self.server = chiefS
+        super(repository,self).__init__()
+        self.reqparser = reqparser.RequestParser()
+        
+        self.reqparser.add_argument('pullStatus', type=int, location = 'json')
+        self.reqparser.add_argument('complexity', type=float, location = 'json')
+        
+        
+    def get(self):
+        args=self.reqparser.parse_args()
+        #repo has not been pulled yet 
+        if args['pullStatus'] == False:
+            print("Got")
+            return {'repo':"https://github.com/bhatnagark/Scaleable-Computing-ChatBot"}
+        
+        if args['pullStatus'] == True:
+            self.server.currentsecondary +=1
+            if self.server.currentsecondary == self.server.secondary:
+                #start time
+                self.server.starttime=time.time()
+            print("WORKER NUMBER: {}".format(self.server.currentsecondary))
+            
+    def post(self):
+        pass
+    
+ api.add_resource(repository, "/repo", endpoint="repo") 
+
+  
 class chiefServer():
     def __init__(self):
         self.secondary=input("Enter the total number of secondary nodes: ")
@@ -47,8 +79,6 @@ class chiefServer():
             
         
     
-
-
 if __name__ == "__main__":
-    chiedS = chiefServer()  # ini an instance of managerServer()
+    chiefS = chiefServer()  # ini an instance of managerServer()
     app.run(port=9000)
