87cf47d740e1ed1682965b55635b84f174a24251
bhatnagark <32340359+bhatnagark@users.noreply.github.com>
2017-12-12 16:50:08 +0000

    Pull request for subordinate

--- subordinate.py
+++ subordinate.py
@@ -8,5 +8,18 @@ Created on Mon Dec 11 23:26:06 2017
 
 import json, requests, subprocess
 
+def run():
+    chiefip=input("Please enter the IP of the chief node")
+    chiefPort=input("Please enter the port of chief node")
+    completecommit=0
+    
+    r=request.get("http://{}:{}/repo".format(chiefip,chiefPort), json={'pullStatus': False})
+    json_data = json.loads(r.text)
+    repoUrl = json_data['repo']
+    subprocess.call(["bash", "subInitScript.sh", repoUrl])
+    
+    r=request.get("http://{}:{}/repo".format(chiefip,chiefPort), json={'pullStatus': True)
+        
+    
 if __name__ == "__main__":
-    work()
+    run()
